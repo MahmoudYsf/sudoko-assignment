d18c89aeeed843193ceeed6baf236bc0055cca16
MahmoudYsf <mahmoud.nabil.ysf1@gmail.com>
2024-08-06 16:54:23 +0300

    Revert "Merge pull request #1 from MahmoudYsf/adding-difficulty"
    This reverts commit 1cf9ea76b57274f71f2c489bd7e309584d0f03e1, reversing
    changes made to 4180bc86ec42e4011a30aa43472ed5899ebcc8e4.

--- Sudoku-App/src/SudokuGrid.tsx
+++ Sudoku-App/src/SudokuGrid.tsx
@@ -9,166 +9,68 @@ import {
   restartGame,
 } from "./GameRules";
 
+// Type definition for the Sudoku grid, a 2D array of numbers
 type Grid = number[][];
 
-const easyGrids: Grid[] = [
-  [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
-  ],
-  [
-    [1, 0, 0, 0, 0, 7, 0, 9, 0],
-    [0, 3, 0, 0, 2, 0, 0, 0, 8],
-    [0, 0, 9, 6, 0, 0, 5, 0, 0],
-    [0, 0, 5, 3, 0, 0, 9, 0, 0],
-    [0, 1, 0, 0, 8, 0, 0, 0, 2],
-    [6, 0, 0, 0, 0, 4, 0, 0, 0],
-    [3, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 4, 0, 0, 0, 0, 0, 0, 7],
-    [0, 0, 7, 0, 0, 0, 3, 0, 0],
-  ],
-  [
-    [0, 0, 0, 0, 0, 0, 2, 0, 0],
-    [0, 0, 0, 6, 0, 0, 0, 0, 3],
-    [0, 4, 8, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 3, 0, 5, 0, 0, 0],
-    [0, 9, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 5, 7, 0],
-    [7, 0, 0, 0, 0, 9, 0, 0, 0],
-    [0, 0, 3, 0, 0, 0, 0, 0, 0],
-  ],
+// Initial Sudoku grid setup
+const initialGrid: Grid = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9],
 ];
 
-const mediumGrids: Grid[] = [
-  [
-    [0, 0, 0, 2, 6, 0, 7, 0, 1],
-    [6, 8, 0, 0, 7, 0, 0, 9, 0],
-    [1, 9, 0, 0, 0, 4, 5, 0, 0],
-    [8, 2, 0, 1, 0, 0, 0, 4, 0],
-    [0, 0, 4, 6, 0, 2, 9, 0, 0],
-    [0, 5, 0, 0, 0, 3, 0, 2, 8],
-    [0, 0, 9, 3, 0, 0, 0, 7, 4],
-    [0, 4, 0, 0, 5, 0, 0, 3, 6],
-    [7, 0, 3, 0, 1, 8, 0, 0, 0],
-  ],
-  [
-    [2, 0, 0, 3, 0, 0, 0, 0, 0],
-    [8, 0, 4, 0, 6, 2, 0, 0, 3],
-    [0, 1, 3, 8, 0, 0, 6, 4, 0],
-    [0, 0, 0, 0, 0, 0, 0, 9, 0],
-    [5, 9, 0, 0, 0, 3, 0, 2, 8],
-    [4, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 6, 1, 0, 0, 7, 3, 5, 0],
-    [7, 0, 0, 9, 0, 0, 0, 0, 1],
-    [0, 3, 0, 0, 0, 1, 0, 0, 0],
-  ],
-  [
-    [0, 2, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 6, 0, 0, 0, 0, 3],
-    [0, 7, 4, 0, 8, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 3, 0, 0, 2],
-    [0, 8, 0, 0, 4, 0, 0, 1, 0],
-    [6, 0, 0, 5, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 7, 8, 0],
-    [5, 0, 0, 0, 0, 9, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 4, 0],
-  ],
-];
-
-const hardGrids: Grid[] = [
-  [
-    [0, 0, 0, 0, 0, 0, 0, 1, 2],
-    [0, 0, 0, 0, 0, 7, 0, 0, 0],
-    [0, 0, 0, 0, 4, 0, 0, 0, 0],
-    [0, 0, 0, 6, 0, 0, 0, 0, 0],
-    [0, 5, 9, 0, 0, 0, 3, 0, 0],
-    [4, 0, 0, 0, 0, 0, 7, 5, 0],
-    [0, 0, 7, 2, 0, 0, 6, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 4, 0, 0, 0, 0, 0],
-  ],
-  [
-    [0, 0, 0, 0, 0, 6, 0, 0, 0],
-    [0, 0, 3, 0, 2, 0, 0, 0, 0],
-    [0, 6, 0, 0, 7, 0, 8, 0, 0],
-    [0, 0, 0, 0, 0, 0, 5, 0, 0],
-    [4, 7, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 2, 0],
-    [0, 4, 0, 0, 0, 0, 0, 3, 0],
-    [0, 0, 5, 0, 0, 9, 0, 0, 0],
-    [0, 2, 0, 0, 0, 0, 0, 0, 0],
-  ],
-  [
-    [0, 0, 0, 8, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 7, 0, 0, 0],
-    [0, 2, 0, 0, 0, 0, 6, 0, 0],
-    [0, 0, 0, 5, 0, 0, 0, 0, 0],
-    [0, 6, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 3, 0, 0, 0],
-    [4, 0, 0, 0, 0, 0, 0, 0, 8],
-    [0, 0, 7, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ],
-];
-
+// Main SudokuGrid component
 const SudokuGrid: React.FC = () => {
+  // State hooks for grid, selected cell, mistakes, game over status, and confetti display
   const [grid, setGrid] = useState<Grid>(() => {
+    // Initialize grid from localStorage or use initialGrid if no saved state
     const savedGrid = localStorage.getItem("sudokuGrid");
-    return savedGrid ? JSON.parse(savedGrid) : easyGrids[0];
-  });
-
-  const [initialGrid, setInitialGrid] = useState<Grid>(() => {
-    const savedInitialGrid = localStorage.getItem("sudokuInitialGrid");
-    return savedInitialGrid ? JSON.parse(savedInitialGrid) : easyGrids[0];
+    return savedGrid ? JSON.parse(savedGrid) : initialGrid;
   });
 
   const [selectedCell, setSelectedCell] = useState<[number, number] | null>(
     null
   );
 
-  const [highlightedNumber, setHighlightedNumber] = useState<number | null>(
-    null
-  );
-
   const [mistakes, setMistakes] = useState(() => {
+    // Initialize mistakes from localStorage or default to 0
     const savedMistakes = localStorage.getItem("sudokuMistakes");
     return savedMistakes ? parseInt(savedMistakes) : 0;
   });
 
   const [gameOver, setGameOver] = useState(() => {
+    // Initialize gameOver status from localStorage or default to false
     const savedGameOver = localStorage.getItem("sudokuGameOver");
     return savedGameOver ? JSON.parse(savedGameOver) : false;
   });
 
   const [showConfetti, setShowConfetti] = useState(false);
 
-  const [difficulty, setDifficulty] = useState<"easy" | "medium" | "hard">(
-    "easy"
-  );
-
-  const [prevGridIndex, setPrevGridIndex] = useState<number | null>(null);
-
   useEffect(() => {
+    // Evaluate the game status and save the current game state whenever grid, mistakes, or gameOver change
+    console.log("Calling Evaluate game status from useEffect");
     evaluateGameStatus(mistakes, setGameOver);
-    saveGameState(grid, initialGrid, mistakes, gameOver);
-  }, [grid, initialGrid, mistakes]);
+    saveGameState(grid, mistakes, gameOver);
+  }, [grid, mistakes]);
 
+  // Function to handle changes in cell values
   const handleChange = (row: number, col: number, value: string) => {
+    // Return early if the game is over
     if (gameOver) return;
 
+    // Validate and update the grid with the new value
     if (value === "" || (parseInt(value) >= 1 && parseInt(value) <= 9)) {
       const newGrid = [...grid];
       newGrid[row][col] = value === "" ? 0 : parseInt(value);
       setGrid(newGrid);
 
+      // Check if the move is valid; if not, increment mistakes count
       if (value !== "") {
         const isValidMove = checkValidMove(newGrid, row, col, parseInt(value));
         if (!isValidMove) {
@@ -176,75 +78,50 @@ const SudokuGrid: React.FC = () => {
         }
       }
 
+      // Check if the game is won and trigger confetti if so
       if (checkWin(newGrid)) {
         setShowConfetti(true);
         setTimeout(() => setShowConfetti(false), 6000);
+
+        // Hide confetti after 6 seconds
       }
     }
   };
 
+  // Function to handle cell click events
   const handleCellClick = (row: number, col: number) => {
-    if (!gameOver) {
+    // Allow cell selection only if the game is not over and the cell is initially empty
+    if (!gameOver && initialGrid[row][col] === 0) {
       setSelectedCell([row, col]);
-      const clickedValue = grid[row][col];
-      setHighlightedNumber(clickedValue !== 0 ? clickedValue : null);
     }
   };
 
-  const selectGrid = (difficulty: "easy" | "medium" | "hard") => {
-    let grids;
-    switch (difficulty) {
-      case "easy":
-        grids = easyGrids;
-        break;
-      case "medium":
-        grids = mediumGrids;
-        break;
-      case "hard":
-        grids = hardGrids;
-        break;
-    }
-    let newIndex;
-    do {
-      newIndex = Math.floor(Math.random() * grids.length);
-    } while (newIndex === prevGridIndex);
-
-    setPrevGridIndex(newIndex);
-    return grids[newIndex];
-  };
-
-  const handleRestart = (newDifficulty: "easy" | "medium" | "hard") => {
-    setDifficulty(newDifficulty);
-    const newGrid = selectGrid(newDifficulty);
-    setInitialGrid(newGrid);
-    restartGame(newGrid, setGrid, setMistakes, setSelectedCell, setGameOver);
-    setHighlightedNumber(null);
+  // Function to handle game restart
+  const handleRestart = () => {
+    // Restart the game with the initial grid and reset state
+    restartGame(
+      initialGrid,
+      setGrid,
+      setMistakes,
+      setSelectedCell,
+      setGameOver
+    );
   };
 
   return (
     <div className={styles.container}>
-      <label htmlFor="difficulty-select">Select Difficulty:</label>
-      <select
-        id="difficulty-select"
-        value={difficulty}
-        onChange={(e) =>
-          handleRestart(e.target.value as "easy" | "medium" | "hard")
-        }
-        className={styles.difficultySelect}
-      >
-        <option value="easy">Easy</option>
-        <option value="medium">Medium</option>
-        <option value="hard">Hard</option>
-      </select>
       <div className={styles.grid}>
+        {/* Render the Sudoku grid */}
         {grid.map((row, rowIndex) =>
           row.map((cell, colIndex) => {
+            // Determine if a cell is editable or has errors
             const isEditable = initialGrid[rowIndex][colIndex] === 0;
             const isIncorrect =
               isEditable &&
               cell !== 0 &&
               !checkValidMove(grid, rowIndex, colIndex, cell);
 
+            // Determine cell styling based on its state
             const cellClass = `${styles.input} ${
               isEditable
                 ? isIncorrect
@@ -257,12 +134,9 @@ const SudokuGrid: React.FC = () => {
               selectedCell[1] === colIndex
                 ? styles.selected
                 : ""
-            } ${
-              highlightedNumber !== null && cell === highlightedNumber
-                ? styles.highlighted
-                : ""
             }`;
 
+            // Determine cell border styling based on row and column index
             const borderClass = `${styles.cell} ${
               (colIndex + 1) % 3 === 0 && colIndex !== 8
                 ? styles.thickBorderRight
@@ -282,8 +156,8 @@ const SudokuGrid: React.FC = () => {
                     handleChange(rowIndex, colIndex, e.target.value)
                   }
                   onClick={() => handleCellClick(rowIndex, colIndex)}
-                  readOnly={!isEditable || gameOver}
-                  className={cellClass}
+                  readOnly={!isEditable || gameOver} // Make input readonly if not editable or game is over
+                  className={cellClass} // Apply cell styling
                 />
               </div>
             );
@@ -296,10 +170,7 @@ const SudokuGrid: React.FC = () => {
           You can't proceed with the game. Please restart the game.
         </div>
       )}
-      <button
-        className={styles.restartButton}
-        onClick={() => handleRestart(difficulty)}
-      >
+      <button className={styles.restartButton} onClick={handleRestart}>
         Restart
       </button>
       {showConfetti && (
@@ -307,11 +178,30 @@ const SudokuGrid: React.FC = () => {
           width={window.innerWidth}
           height={window.innerHeight}
           recycle={false}
-          numberOfPieces={1000}
+          numberOfPieces={10000}
           gravity={0.1}
+          colors={[
+            "#f44336",
+            "#e91e63",
+            "#9c27b0",
+            "#673ab7",
+            "#3f51b5",
+            "#2196f3",
+            "#03a9f4",
+            "#00bcd4",
+            "#009688",
+            "#4CAF50",
+            "#8BC34A",
+            "#CDDC39",
+            "#FFEB3B",
+            "#FFC107",
+            "#FF9800",
+            "#FF5722",
+          ]}
         />
       )}
     </div>
   );
 };
+
 export default SudokuGrid;
